f08e0cbccb5bc5a52c2e4642d2cb67f88ee9e56c
Kyrylo Lytvynov <themaiby0@gmail.com>
2026-02-25 01:00:11 +0200

    chore(dependencies): update packages metadata (#148)

--- libs/config/src/tokens/index.ts
+++ libs/config/src/tokens/index.ts
@@ -1,5 +1,3 @@
-import { Brand, ServiceToken } from '@zerly/core';
+export const ENV_METADATA_KEY = Symbol('env-metadata');
 
-export const ENV_METADATA_KEY = Symbol('env-metadata') as ServiceToken<Brand.Config>;
-
-export const APP_CONFIG = Symbol('app-config') as ServiceToken<Brand.Config>;
+export const APP_CONFIG = Symbol('app-config');
