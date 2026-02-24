b73c92a97fb5b0b4f4ae4611164c708bec770d6a
Kyrylo Lytvynov <themaiby0@gmail.com>
2026-02-25 01:02:37 +0200

    chore(merge): main (#149)

--- libs/microservice/src/const.ts
+++ libs/microservice/src/const.ts
@@ -1,11 +1,5 @@
-import { ServiceToken } from '@zerly/core';
+export const MICROSERVICE_SERVER_PROVIDER = Symbol('MICROSERVICE_SERVER_PROVIDER');
 
-export const MICROSERVICE_SERVER_PROVIDER = Symbol(
-  'MICROSERVICE_SERVER_PROVIDER',
-) as ServiceToken<'microservice'>;
+export const MICROSERVICE_CLIENT_PROVIDER = Symbol('MICROSERVICE_CLIENT_PROVIDER');
 
-export const MICROSERVICE_CLIENT_PROVIDER = Symbol(
-  'MICROSERVICE_CLIENT_PROVIDER',
-) as ServiceToken<'microservice'>;
-
-export const MICROSERVICE_OPTIONS = Symbol('MICROSERVICE_OPTIONS') as ServiceToken<'microservice'>;
+export const MICROSERVICE_OPTIONS = Symbol('MICROSERVICE_OPTIONS');
