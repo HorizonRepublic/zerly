a3220ce2d39ea2ab71474ca0432f25bb325830b9
Kyrylo Lytvynov <themaiby0@gmail.com>
2026-03-24 03:36:56 +0200

    feat(config): add ConfigResolverFactory

---
+++ libs/config/src/__tests__/config-resolver.factory.spec.ts
@@ -0,0 +1,21 @@
+import { ConfigFormat } from '../enums/config-format.enum';
+import { ConfigResolverFactory } from '../resolvers/config-resolver.factory';
+import { EnvResolver } from '../resolvers/env.resolver';
+import { YamlResolver } from '../resolvers/yaml.resolver';
+
+describe('ConfigResolverFactory', () => {
+  it('should create EnvResolver for dotenv format', () => {
+    const resolver = ConfigResolverFactory.create(ConfigFormat.Dotenv);
+    expect(resolver).toBeInstanceOf(EnvResolver);
+  });
+
+  it('should create YamlResolver for yaml format', () => {
+    const resolver = ConfigResolverFactory.create(ConfigFormat.Yaml, '/some/path.yaml');
+    expect(resolver).toBeInstanceOf(YamlResolver);
+  });
+
+  it('should create EnvResolver when no format specified', () => {
+    const resolver = ConfigResolverFactory.create();
+    expect(resolver).toBeInstanceOf(EnvResolver);
+  });
+});
---
+++ libs/config/src/resolvers/config-resolver.factory.ts
@@ -0,0 +1,31 @@
+import { ConfigFormat } from '../enums/config-format.enum';
+import { IConfigResolver } from './config-resolver.interface';
+import { EnvResolver } from './env.resolver';
+import { YamlResolver } from './yaml.resolver';
+
+/**
+ * Factory for creating the appropriate `IConfigResolver` based on format.
+ */
+export class ConfigResolverFactory {
+  /**
+   * Creates a config resolver instance.
+   * @param format - The configuration format. Defaults to `ConfigFormat.Dotenv`.
+   * @param defaultPath - Default YAML file path. Required when `format` is `Yaml`.
+   * @returns A resolver instance matching the requested format.
+   */
+  public static create(
+    format: ConfigFormat = ConfigFormat.Dotenv,
+    defaultPath?: string,
+  ): IConfigResolver {
+    switch (format) {
+      case ConfigFormat.Yaml: {
+        return new YamlResolver(defaultPath ?? '');
+      }
+
+      case ConfigFormat.Dotenv:
+      default: {
+        return new EnvResolver();
+      }
+    }
+  }
+}
