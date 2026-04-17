3a556ec5467b98bdad881b873534336ffc9641d1
Kyrylo Lytvynov <themaiby0@gmail.com>
2026-04-17 08:29:11 +0300

    chore: jets preset warning supressing

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,13 @@
 const nxPreset = require('@nx/jest/preset').default;
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  workerIdleMemoryLimit: '512MB',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
+};
+
+// Suppress "ExperimentalWarning: VM Modules" noise from Jest + Node 22+.
+// Applied once at preset level so every lib inherits it.
+process.env.NODE_NO_WARNINGS = '1';
